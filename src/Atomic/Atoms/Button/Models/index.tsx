8160337221c32423d171098e9da047809b3a5913
Amilcar-junior <amilcarjuniordev@gmail.com>
2022-07-04 17:05:28 -0300

    create settings colors and enrionments

--- src/Atomic/Atoms/Button/Models/index.tsx
+++
@@ -1,9 +0,0 @@
-export interface PropsButtonAdd {
-  width?: string | number;
-  height?: string | number;
-  borderRadius?: string | number;
-  backgroundColor?: string;
-  alignItems?: 'center' | 'flex-start' | 'flex-end';
-  justifyContent?: 'center' | 'flex-start' | 'flex-end';
-  flexDirection?: 'row' | 'column';
-}
---
+++ src/Settings/environment.ts
@@ -0,0 +1,9 @@
+export const FONT_SIZE_SMALLER = 10;
+export const FONT_SIZE_SMALL = 12;
+export const FONT_SIZE_AVERAGE = 13;
+export const FONT_SIZE_BIG = 16;
+export const FONT_SIZE_XBIG = 18;
+export const FONT_SIZE_XXBIG = 20;
+export const BORDER_RADIUS_NONE = 0;
+export const BORDER_READIUS_MEDIUM = 10;
+export const BORDER_RADIUS_BIG = 20;
